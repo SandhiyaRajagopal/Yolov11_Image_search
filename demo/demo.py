d5d51c9875679b5d7ccfbb2cda25a5acd0e36716
Sandhiya <rajagopalsandhiya272@gmail.com>
2026-05-25 19:18:47 +0530

    Initial commit

---
+++ demo/demo.py
@@ -0,0 +1,8 @@
+# python streamlit_basics.py
+# streamlit run test/streamlit_basics.py
+
+import streamlit as st
+
+# basics element
+st.title("Streamlit Intro")
+st.write("This is where we'll learn streamlit")
